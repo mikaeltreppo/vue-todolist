59ed298f8a4eee7b589ff9e60e714290b678464e
mikaeltreppo <mikaeltreppo@outlook.com>
2023-03-16 16:27:50 +0100

    aggiunta interattività

--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-     
+    data() {
+        return {
+            listTasks: [
+                {
+                    name: "Lavatrice",
+                    bool: ""
+                },
+                {
+                    name: "Cucinare",
+                    bool: ""
+                }, 
+                {
+                    name: "Nuotare",
+                    bool: ""
+                },
+                 {
+                    name: "Mangiare",
+                    bool: ""
+                },
+                 {
+                    name: "Lavarsi",
+                    bool: ""
+                },
+            ],
+            activeTask : 0,
+            newItem:"",
+        
+        }
+    },
+    methods: {
+        addItem(){
+            let itemNew = {
+                name:this.newItem,
+                bool:""
+            }
+            this.listTasks.push(itemNew);
+            this.newItem = ""
+        },
+        remove(index){    console.log(newItem),
+            this.listTasks.splice(index, 1)
+        }
     }
-  }
 }).mount('#app')
